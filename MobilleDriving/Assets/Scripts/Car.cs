7334d2a569a1783df8513057bb1b8c3bcc3c50ad
artextrim5 <artextrim5@yandex.ru>
2021-09-30 19:40:01 +0300

    Добавил пак с графикой, написал механику передвижения и поворота.

---
+++ MobilleDriving/Assets/Scripts/Car.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    [SerializeField] float movementSpeed = 10f;
+    [SerializeField] float speedIncrees = 0.1f;
+    [SerializeField] float turnSpeed = 200f;
+    SceneLouder sceneLouder;
+
+    private int steerValue;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneLouder = FindObjectOfType<SceneLouder>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CarMoveForward();
+        CarTurning();
+    }
+
+    private void CarTurning()
+    {
+        transform.Rotate(0f, steerValue * turnSpeed * Time.deltaTime, 0f);
+    }
+
+    private void CarMoveForward()
+    {
+        movementSpeed += speedIncrees * Time.deltaTime;
+        transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+    }
+
+    public void Steer(int value)
+    {
+        steerValue = value;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Box"))
+        {
+            Debug.Log("1");
+            sceneLouder.LoudMainManu();
+        }
+    }
+
+}
